959cdec31554c916bfcab7b05994c882764c10a5
Ganesh12345 <aksatpute71@gmail.com>
2022-06-16 16:38:24 +0530

    PHON-03:create springboot project with required dependency

---
+++ src/main/java/com/bikkadIt/PhoneBookApplication/PhoneBookkApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIt.PhoneBookApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneBookkApplication.class, args);
+	}
+
+}
